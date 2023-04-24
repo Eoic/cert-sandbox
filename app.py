924df4749752b6a0d09b3f01e6678ef4fab29ba5
idf_karolisstr <karolis.strazdas@indeform.com>
2023-04-24 13:33:45 +0300

    Update gunicorn setup.

--- app.py
+++ app.py
@@ -10,7 +10,3 @@ def index():
 @app.route(f'/.well-known/acme-challenge/{os.environ.get("ACME_CHALLENGE_URL_KEY")}')
 def acme_challenge():
     return os.environ.get("ACME_CHALLENGE_RESPONSE")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
