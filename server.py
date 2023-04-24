520da9e1812b4b7999c57e0a95ce67ff3322b2b8
idf_karolisstr <karolis.strazdas@indeform.com>
2023-04-24 13:15:54 +0300

    Update server script.

--- server.py
+++ server.py
@@ -3,11 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/.well-known/acme-challenge/<copy last part of URL from certbot console>")
-# e.g. /.well-known/acme-challenge/lzrajCaq8vbw5Qz2o_XXXXXXXXXXXXXXXXX
-def hello():
-    return "<copy data from certbot console>"
-    # e.g. return "lzrajCaq8vbw5Qz2o_XXXXXXXXXXXXXXXXXXz4RnfWtLKaIc6EdhsOsr4fb6RFZuUoabZW5dPW36cmc"
+@app.route(f'/.well-known/acme-challenge/{os.environ.get("ACME_CHALLENGE_URL_KEY")}')
+def acme_challenge():
+    return os.environ.get("ACME_CHALLENGE_RESPONSE")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
