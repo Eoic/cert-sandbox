c84abfb94ac84a57757084bc1ed3828f3a4c0366
idf_karolisstr <karolis.strazdas@indeform.com>
2023-04-24 13:18:06 +0300

    Add index route.

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return {'status': 'OK'}
+
 @app.route(f'/.well-known/acme-challenge/{os.environ.get("ACME_CHALLENGE_URL_KEY")}')
 def acme_challenge():
     return os.environ.get("ACME_CHALLENGE_RESPONSE")
